b7c719f1f7e4ecd615b9e7cb0f3e693f388d1039
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2017-08-31 10:25:30 +0200

    Set linger to 0 was a bad idea
    Messages get lost on a service restart

--- src/msg-bus/Channel.cpp
+++ src/msg-bus/Channel.cpp
@@ -66,9 +66,6 @@ ChannelFactory::~ChannelFactory()
 std::unique_ptr<Producer> ChannelFactory::createProducer(const std::string &name, bool listen)
 {
     zmq::socket_t zmqSocket(zmqContext, ZMQ_PUSH);
-    int linger = 0;
-    zmqSocket.setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
-
     auto fullPath = baseDir / name;
     std::string address = std::string("ipc://") + fullPath.native();
 
@@ -86,10 +83,6 @@ std::unique_ptr<Producer> ChannelFactory::createProducer(const std::string &name
 std::unique_ptr<Consumer> ChannelFactory::createConsumer(const std::string &name, bool listen)
 {
     zmq::socket_t zmqSocket(zmqContext, ZMQ_PULL);
-
-    int linger = 0;
-    zmqSocket.setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
-
     auto fullPath = baseDir / name;
     std::string address = std::string("ipc://") + fullPath.native();
 
