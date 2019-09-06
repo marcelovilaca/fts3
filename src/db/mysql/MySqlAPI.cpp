7b6185c6c0e832961eb2b0d54fceeea23ed4f29c
Edward Karavakis <edward.karavakis@cern.ch>
2019-09-06 15:52:22 +0000

    Improve scheduler performance

--- src/db/mysql/MySqlAPI.cpp
+++ src/db/mysql/MySqlAPI.cpp
@@ -459,7 +459,11 @@ void MySqlAPI::getQueuesWithPending(std::vector<QueueId>& queues)
            "SELECT f.vo_name, f.source_se, f.dest_se FROM t_file f "
            "WHERE f.file_state = 'SUBMITTED' "
            "GROUP BY f.source_se, f.dest_se, f.file_state, f.vo_name "
-           "ORDER BY null");
+           "ORDER BY null",
+           soci::use(hashSegment.start),
+           soci::use(hashSegment.end)
+		   );
+
 
         soci::statement stmt1 = (sql.prepare <<
              "SELECT file_id FROM t_file "
